0765e914c7bf04edad9597faf4fb3798d59ed2dc
wjdgotjd529 <wjdgotjd529@naver.com>
2022-07-05 20:53:03 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,43 +1,26 @@
 "use strict";
 
-const output = {
-    home: (req, res) => {
-        res.render("home/index");
-    },
-    
-    login: (req, res) => {
-        res.render("home/login");
-    }
-};
+const User = require("../../models/User");
 
-const users = {
-    id: ["woorimIT", "나개발", "김팀장"],
-    psword: ["1234", "1234", "123456"]
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
 const process = {
-    login: (req, res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
-
-            if (users.id.includes(id)) {
-                const idx = users.id.indexOf(id);
-                if (users.psword[idx] === psword){
-                    return res.json({
-                        success: true,
-                    });
-                }
-            }
-
-            return res.json({
-                success: false,
-                msg: "로그인에 실패하였습니다.",
-            });
-    }
-}
-
+  login: (req, res) => {
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
+  },
+};
 
 module.exports = {
-    output,
-    process
+  output,
+  process,
 };
